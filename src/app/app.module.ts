8d49745378ed555d3013e13c1ae9629579689ccb
jic900 <jiachen.can@gmail.com>
2017-03-17 02:32:30 -0600

    qiz264 - set up project structure for initial prototyping (home, menu, and register/login)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HomeComponent } from './features/home/home.component';
+import { RegisterComponent } from './features/auth/register.component';
+import { LoginComponent } from './features/auth/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
