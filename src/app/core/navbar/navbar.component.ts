fc688806b715d0cf585326cf61037b2e3d72d8b2
jic900 <jiachen.can@gmail.com>
2017-04-07 02:43:43 -0600

    qiz264 - submenu animation working, expand issue remaining.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -7,59 +7,89 @@ import { AppConfig } from 'app/config/app.config';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   animations: [
-    trigger('slideInOut', [
+    trigger('menuInOut', [
       state('2', style({transform: 'translateX(0)'})),
       state('1', style({transform: 'translateX(-100%)'})),
       transition('1 => 2', animate('500ms ease-in-out')),
       transition('2 => 1', animate('500ms ease-in-out'))
     ]),
+    // trigger('subMenuInOut', [
+    //   state('2', style({transform: 'translateY(0)'})),
+    //   state('1', style({transform: 'translateY(-2em)'})),
+    //   transition('* => 2', animate('500ms ease-in-out')),
+    //   transition('2 => 1', animate('500ms ease-in-out'))
+    // ]),
   ],
 })
 
 export class NavbarComponent {
 
   homeLogo = AppConfig.HOME_LOGO;
-  state;
+  menuState;
+  subMenuState;
   width;
   constructor() {
     this.width = window.innerWidth;
-    this.updateState();
+    this.updateStates();
   }
 
   @HostListener('window:resize', ['$event'])
   onWindowResize(event) {
     this.width = event.target.innerWidth;
     console.log('width =>', this.width);
-    this.updateState();
+    this.updateStates();
   }
 
-  private updateState() {
+  private updateStates() {
     if (this.width > 768) {
-      this.state = 0;
-    } else if (! this.state) {
-      this.state = 1;
+      this.menuState = 0;
+      this.subMenuState = 0;
+    } else if (! this.menuState) {
+      this.menuState = 1;
+      this.subMenuState = 1;
     }
   }
 
-  toggleState() {
-    console.log('toggleState before => this.state === ' + this.state);
-    this.state = this.state !== 2 ? 2 : 1;
-    console.log('toggleState after => this.state === ' + this.state);
+  toggleMenuState() {
+    console.log('toggleMenuState before => this.menuState === ' + this.menuState);
+    if (this.menuState === 2 && this.subMenuState === 2) {
+      this.subMenuState = 1;
+    }
+    this.menuState = this.menuState !== 2 ? 2 : 1;
+    console.log('toggleMenuState after => this.menuState === ' + this.menuState);
+  }
+  isMenuCollapsed() {
+    console.log('isMenuCollapsed => this.menuState === ' + this.menuState);
+    return this.menuState === 1;
+  }
+  isMenuExpanded() {
+    console.log('isMenuExpanded => this.menuState === ' + this.menuState);
+    return this.menuState === 2;
   }
 
-  isCollapsed() {
-    return this.state === 1;
+  toggleSubMenuState() {
+    console.log('toggleSubMenuState before => this.subMenuState === ' + this.subMenuState);
+    this.subMenuState = this.subMenuState !== 2 ? 2 : 1;
+    console.log('toggleSubMenuState after => this.subMenuState === ' + this.subMenuState);
   }
-
-  isExpanded() {
-    console.log('isExpanded => this.state === ' + this.state);
-    return this.state === 2;
+  isSubMenuCollapsed() {
+    console.log('isSubMenuCollapsed => this.subMenuState === ' + this.subMenuState);
+    return this.subMenuState === 1;
+  }
+  isSubMenuExpanded() {
+    console.log('isSubMenuExpanded => this.subMenuState === ' + this.subMenuState);
+    return this.subMenuState === 2;
   }
 
   // state;
   // constructor() {
-  //   this.state = 1;
+  //   if (window.innerWidth > 768) {
+  //     this.state = 0;
+  //   } else {
+  //     this.state = 1;
+  //   }
   // }
+  //
   // toggleState() {
   //   console.log('toggleState before => this.state === ' + this.state);
   //   this.state = this.state === 1 ? 2 : 1;
